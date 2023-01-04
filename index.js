ca174767fb124dfd107782c239cc1cc4d5a6743f
KRNDash <barbash.elizaveta@yandex.ru>
2023-01-04 17:29:01 +0800

    Динамическое создание списка добавлено

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 document.querySelector("#test-btn-js").addEventListener("click", function () {
-  let doc = document.querySelector(".test_page");
-  console.log("Запущено");
+  const iframe = document.querySelector(".test_page iframe");
+  const innerDoc = iframe.contentDocument
+    ? iframe.contentDocument
+    : iframe.contentWindow.document;
 
-  let sizeStart = 32;
-  let sizeEnd = 150;
+  const sizeStart = 32;
+  const sizeEnd = 150;
 
-  let rule1 = checkTitleSize(doc, sizeStart, sizeEnd);
-  document.getElementById("rule1").checked = rule1;
-  console.log(rule1);
+  const result = [];
+
+  const rule1 = checkTitleSize(innerDoc, sizeStart, sizeEnd);
+  result.push({
+    rule: "Размер шрифта Заголовоков (h1-h6) от 32 до 190px",
+    result: `<input type="checkbox" id="rule1" name="rule1" ${
+      rule1 ? "checked" : ""
+    } disabled />`,
+  });
+
+  printResult(result);
 });
 
-//1. Размер шрифта Заголовоков (h1-h6) от 32 до 150px
+import config from "./rules.json";
 
+const configObject = JSON.parse(config);
+configObject.forEach((rule) => {
+  const element = document.querySelectorAll(rule.selector);
+  window.getComputedStyle(rule.valueToCheck);
+});
 //js doc
 /**
- *
  * @param {Element | null} html
  * @param {number} sizeStart
  * @param {number} sizeEnd
@@ -34,7 +48,31 @@ function checkTitleSize(html, sizeStart = 32, sizeEnd = 150) {
 
     if (style < sizeStart || style > sizeEnd) {
       result = false;
+      console.log(title.outerHTML);
     }
   });
   return result;
+}
+
+//
+//
+//Вывод результатов проверки в таблицу
+function printResult(result) {
+  document.querySelector(".table").innerHTML = `
+  <thead>
+      <tr>
+          <th scope="col">Критерий</th>
+          <th scope="col">Результат</th>
+      </tr>
+  </thead>
+  <tbody>
+  ${result.map((res) =>
+    `
+    <tr>
+      <td>${res.rule}</td>
+      <td>${res.result}</td>
+    </tr>
+  `.trim()
+  )}
+  </tbody>`.trim();
 }
