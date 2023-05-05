340feec5c3b085985f4d14c99ffef78f92c2f5ef
Elizabeth <barbash.elizaveta@yandex.ru>
2023-05-05 13:38:35 +0800

    Многостраничный сайт и шапка

---
+++ src/Edit.tsx
@@ -0,0 +1,14 @@
+import "./styles/style.css";
+import "./styles/header-style.css";
+
+function Edit() {
+    return (
+        <div>
+            <h1>
+            Редактирование правил
+            </h1>
+        </div>
+    );
+}
+
+export default Edit;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,52 +1,88 @@
-import "../styles/header-style.css";
-import "../utils/header";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {Link} from 'react-router-dom'
 
-export function Header() {
+function Header() {
   return (
-    <>
-      <header className="cd-header">
-        <div className="header-wrapper">
-          <div className="logo-wrap">
-            <a href="#" className="hover-target">
-              <img className="logo" src="src/assets/images/logo.svg" alt="" />
-            </a>
-          </div>
-          <div className="nav-but-wrap">
-            <div className="menu-icon hover-target">
-              <span className="menu-icon__line menu-icon__line-left"></span>
-              <span className="menu-icon__line"></span>
-              <span className="menu-icon__line menu-icon__line-right"></span>
-            </div>
-          </div>
-        </div>
-      </header>
-
-      <div className="nav">
-        <div className="nav__content">
-          <ul className="nav__list">
-            <li className="nav__list-item active-nav">
-              <a href="#" className="hover-target">
-                home
-              </a>
-            </li>
-            <li className="nav__list-item">
-              <a href="#" className="hover-target">
-                studio
-              </a>
-            </li>
-            <li className="nav__list-item">
-              <a href="#" className="hover-target">
-                news
-              </a>
-            </li>
-            <li className="nav__list-item">
-              <a href="#" className="hover-target">
-                contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </>
+    <Navbar bg="light" expand="lg">
+      <Container>
+      <Navbar.Brand href="/">
+            <img
+              alt="Logo"
+              src="src/assets/images/logo.svg"
+              width="40"
+              height="auto"
+              className="d-inline-block align-top"
+            />
+          </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link><Link to='/'>Протестировать страницу</Link></Nav.Link>
+            <Nav.Link><Link to='/add'>Добавить правило</Link></Nav.Link>
+            <Nav.Link><Link to='/edit'>Изменить правило</Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
+
+export default Header;
+
+
+
+
+// import "../styles/header-style.css";
+// import "../utils/header";
+
+// export function Header() {
+//   return (
+//     <>
+//       <header className="cd-header">
+//         <div className="header-wrapper">
+//           <div className="logo-wrap">
+//             <a href="#" className="hover-target">
+//               <img className="logo" src="src/assets/images/logo.svg" alt="" />
+//             </a>
+//           </div>
+//           <div className="nav-but-wrap">
+//             <div className="menu-icon hover-target">
+//               <span className="menu-icon__line menu-icon__line-left"></span>
+//               <span className="menu-icon__line"></span>
+//               <span className="menu-icon__line menu-icon__line-right"></span>
+//             </div>
+//           </div>
+//         </div>
+//       </header>
+
+//       <div className="nav">
+//         <div className="nav__content">
+//           <ul className="nav__list">
+//             <li className="nav__list-item active-nav">
+//               <a href="#" className="hover-target">
+//                 home
+//               </a>
+//             </li>
+//             <li className="nav__list-item">
+//               <a href="#" className="hover-target">
+//                 studio
+//               </a>
+//             </li>
+//             <li className="nav__list-item">
+//               <a href="#" className="hover-target">
+//                 news
+//               </a>
+//             </li>
+//             <li className="nav__list-item">
+//               <a href="#" className="hover-target">
+//                 contact
+//               </a>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
